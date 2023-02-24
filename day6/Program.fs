0493bc64b7b5d695a154b6459299b4be9af8c2f9
r-tamura <rtamura30@gmail.com>
2023-02-24 23:01:21 +0900

    day6 part1

---
+++ day6/Program.fs
@@ -0,0 +1,42 @@
+﻿let areDifferentAllChars (s: string) : bool =
+    let uniqueChars = s |> Set.ofSeq |> List.ofSeq
+    s.Length = uniqueChars.Length
+
+
+let findStartOfPacketMarker (stream: string) (packageLength: int) =
+    let rec findStartOfPacketMarker' i =
+        let cand = stream[i .. i + packageLength - 1]
+
+        if areDifferentAllChars cand then
+            (i, cand)
+        else
+            findStartOfPacketMarker' (i + 1)
+
+    findStartOfPacketMarker' 0
+
+
+let getEndOfPacketMarkerIndex (start: int) (packet: string) : int = start + packet.Length
+
+let MARKER_PACKET_LENGTH = 4
+
+let part1 stream =
+    (stream, MARKER_PACKET_LENGTH)
+    ||> findStartOfPacketMarker
+    ||> getEndOfPacketMarkerIndex
+    |> printfn "%A"
+
+[<EntryPoint>]
+let main argv =
+    let filepath, part =
+        match argv with
+        | [| filepath; part |] -> (filepath, part)
+        | _ -> failwith "usage: dotnet run <filepath> <part>"
+
+
+    let stream = System.IO.File.ReadAllText filepath
+
+    match part with
+    | "1" -> part1 stream
+    | _ -> failwith "part is '1' or '2'"
+
+    0
